444cd8c0f476e2d4b8a1d5f9e87bcc09a71405c9
Christophe Jolif <cjolif@gmail.com>
2014-03-25 18:26:29 +0100

    translation center needs dos newline

--- extract-nls.sh
+++ extract-nls.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+find . -type d -name "nls" -not \( -path "*/node_modules/*" -o -path "*/cldr/*" -o -path "*/tests/*" \) -print0 |  xargs -0 -i"{}" find "{}" -maxdepth 1 -name "*.js" -print0 | xargs -0 -i"{}" unix2dos "{}" 
 find . -type d -name "nls" -not \( -path "*/node_modules/*" -o -path "*/cldr/*" -o -path "*/tests/*" \) -print0 |  xargs -0 -i"{}" find "{}" -maxdepth 1 -name "*.js" -print0 | tar --null -T- -zcvf nls.tar.gz
  #--no-wildcards-match-slash

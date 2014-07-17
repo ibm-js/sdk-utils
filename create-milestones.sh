bb5c0f1399a4a88f05811a70e54d951d99ea6915
Christophe Jolif <cjolif@gmail.com>
2014-07-17 10:56:14 +0200

    update doc

--- create-milestones.sh
+++ create-milestones.sh
@@ -2,8 +2,9 @@
 # $1 = API Token
 # $2 = User Name
 # $3 = Repo Name
-curl -H "Authorization: token $1" -d "{\"title\":\"1.0\",\"due_on\":\"2014-07-24T23:39:01Z\"}" https://api.github.com/repos/$2/$3/milestones
-curl -H "Authorization: token $1" -d "{\"title\":\"1.0alpha\",\"due_on\":\"2014-03-27T23:39:01Z\"}" https://api.github.com/repos/$2/$3/milestones
-curl -H "Authorization: token $1" -d "{\"title\":\"1.0beta\",\"due_on\":\"2014-06-12T23:39:01Z\"}" https://api.github.com/repos/$2/$3/milestones
-curl -H "Authorization: token $1" -d "{\"title\":\"1.1\"}" https://api.github.com/repos/$2/$3/milestones
+curl -H "Authorization: token $1" -d "{\"title\":\"0.2.0-dev\",\"due_on\":\"2014-08-01T23:39:01Z\"}" https://api.github.com/repos/$2/$3/milestones
+curl -H "Authorization: token $1" -d "{\"title\":\"1.0.0\",\"due_on\":\"2015-03-30T23:39:01Z\"}" https://api.github.com/repos/$2/$3/milestones
+curl -H "Authorization: token $1" -d "{\"title\":\"1.0.0-alpha\",\"due_on\":\"2015-01-15T23:39:01Z\"}" https://api.github.com/repos/$2/$3/milestones
+curl -H "Authorization: token $1" -d "{\"title\":\"1.0.0-beta\",\"due_on\":\"2015-02-12T23:39:01Z\"}" https://api.github.com/repos/$2/$3/milestones
+curl -H "Authorization: token $1" -d "{\"title\":\"1.1.0\"}" https://api.github.com/repos/$2/$3/milestones
 

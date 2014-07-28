d56b76ed02c44d723590e16cdef67cc647cd09fe
Clément Mathieu <clement.mathieu@outlook.com>
2014-07-28 16:25:57 +0200

    add build for decor, dcolor, ecma402. Also add a boot file to load the layer.

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -11,91 +11,146 @@ module.exports = function (grunt) {
 	// The grunt.config property populated by amdserialize, containing the
 	// list of files to include in the layer.
 	var outprop = "amdoutput";
-	
-	var libDirs = ["delite", "deliteful", "dpointer", "dtreemap", "liaison"];
-	var libDirsBuild = libDirs.map(function(dir) {
+
+	var libDirs = ["decor", "delite", "deliteful", "dpointer", "dtreemap", "dcolor", "ecma402"/*, "liaison"*/];
+	var libDirsBuild = libDirs.map(function (dir) {
 		return dir + "-build";
 	});
-	
+
+	var decorPatterns = [
+		// Include
+		"decor/*.js",
+		"requirejs-dplugins/has.js",
+		"requirejs-dplugins/i18n.js",
+		// Exclude
+		"!decor/Gruntfile.js",
+		"!decor/keys.js"
+	];
+
 	var delitePatterns = [
 		// Include
-		"delite/**/*.js", 
+		"delite/**/*.js",
+		"dojo/hccss.js",
+		"dojo/string.js",
+		"requirejs-text/text.js",
 		// Exclude
-		"!delite/dijit/**", 
-		"!delite/docs/**",
-		"!delite/form/**",
 		"!delite/Gruntfile.js",
-		"!delite/layout/**",
-		"!delite/mobile/**",
+		"!delite/node_modules/**",
 		"!delite/nls/**",
-		"!delite/samples/**", 
-		"!delite/tests/**", 
-		"!delite/themes/tasks/**", 
-		"!delite/**/holodark/**", 
-		"!delite/**/ios/**"
+		"!delite/samples/**",
+		"!delite/tests/**",
+		"!delite/TooltipDialog.js",
+		"!delite/Tooltip.js",
+		"!delite/Overlay.js",
+		"!delite/Opener.js",
+		"!delite/DialogLevelManager.js",
+		"!delite/DialogBase.js",
+		"!delite/Dialog.js"
 	];
-		
+
+	var delitefulPatterns = [
+		// Include
+		"deliteful/**/*.js",
+		// Exclude
+		"!deliteful/tests/**",
+		"!deliteful/samples/**",
+		"!deliteful/docs/**",
+		"!deliteful/**/holodark/**",
+		"!deliteful/**/ios/**",
+		"!deliteful/Gruntfile.js"
+	];
+
+	var expandFiles = {
+		filter: "isFile"
+	};
+
+	function trimExt(path) {
+		return path.slice(0, -3);
+	}
+
 	grunt.initConfig({
-	// The loader config should go here.
+		// The loader config should go here.
 		amdloader: {
 			baseUrl: "./",
 
 			// Enable build of requirejs-text/text
-			inlineText: true
+			inlineText: true,
+			config: {
+				"ecma402/locales": /^(zh-Han(s|t)|en|es|fr|it|pt|ja|de)$/
+			}
+
 		},
 
 		amdbuild: {
+			buildPlugin: true,
+
 			// dir is the output directory.
 			dir: tmpdir,
 
-			// List of plugins that the build should not try to resolve at build time.
-			runtimePlugins: ["delite/theme", "delite/css", "dojo/has"],
-
 			// List of layers to build.
 			layers: [{
-				name: "delite/layer",
-				include: grunt.file.expand({filter: "isFile"}, delitePatterns).map(function (path) {return path.slice(0,-3);})
-			},{
-				name: "deliteful/layer",
-				includeFiles: ["deliteful/**/*.js"],
-				excludeFiles: ["deliteful/tests/**", "deliteful/samples/**", "deliteful/docs/**", "deliteful/**/holodark/**", "deliteful/**/ios/**", "deliteful/Gruntfile.js"]
-			},{
+				name: "decor/layer",
+				include: grunt.file.expand(expandFiles, decorPatterns).map(trimExt)
+			}, {
 				name: "dpointer/layer",
 				includeFiles: ["dpointer/**/*.js"]
-			},{
+			}, {
+				name: "ecma402/layer",
+				include: ["ecma402/IntlShim"],
+				exclude: ["requirejs-dplugins/has", "requirejs-text/text"]
+			}, {
+				name: "delite/layer",
+				include: grunt.file.expand(expandFiles, delitePatterns).map(trimExt).concat(["delite/theme!delite/themes/{{theme}}/global.css"]),
+				exclude: ["decor/layer", "dpointer/layer", "ecma402/layer"]
+			}, {
+				name: "deliteful/layer",
+				include: grunt.file.expand(expandFiles, delitefulPatterns).map(trimExt),
+				exclude: ["decor/layer", "dpointer/layer", "ecma402/layer", "delite/layer", "dstore/Memory", "dstore/Observable"]
+			}, {
 				name: "dtreemap/layer",
 				includeFiles: ["dtreemap/**/*.js"],
-				excludeFiles: ["dtreemap/tests/**", "dtreemap/demos/**", "dtreemap/docs/**"]
-			},{
-				name: "liaison/layer",
-				includeFiles: ["liaison/**/*.js"],
-				excludeFiles: ["liaison/delite/**", "liaison/polymer/**", "liaison/tests/**", "liaison/samples/**", "liaison/docs/**", "liaison/node_modules/**", "liaison/Gruntfile.js"]
-			},{
-				name: "liaison/delite/layer",
-				includeFiles: ["liaison/delite/**/*.js"],
-				excludeFiles: ["liaison/delite/widgets/StarRating.js"]
-			}]
+				excludeFiles: ["dtreemap/tests/**", "dtreemap/demos/**", "dtreemap/docs/**", "dtreemap/Gruntfile.js"]
+			}, {
+				name: "dcolor/layer",
+				includeFiles: ["dcolor/*.js"]
+			}] //,{
+			// },{
+			// name: "liaison/layer",
+			// includeFiles: ["liaison/**/*.js"],
+			// excludeFiles: ["liaison/delite/**", "liaison/polymer/**", "liaison/tests/**", "liaison/samples/**", "liaison/docs/**", "liaison/node_modules/**", "liaison/Gruntfile.js"]
+			// },{
+			// name: "liaison/delite/layer",
+			// includeFiles: ["liaison/delite/**/*.js"],
+			// excludeFiles: ["liaison/delite/widgets/StarRating.js"]
+			// }]
 		},
 
 		buildLib: {
 			liaison: {
 				samples: {
-					src: ["./samples/**/*", "!./samples/**/loan.html", "!./samples/delite/widgetskitchensink.html", "!./samples/delite-polymer/**/*", "!./samples/polymer/**/*"],
+					src: [
+						"./samples/**/*",
+						"!./samples/**/loan.html",
+						"!./samples/delite/widgetskitchensink.html",
+						"!./samples/delite-polymer/**/*",
+						"!./samples/polymer/**/*"
+					],
 					"./samples/delite/**/*": {
 						sublayers: ["liaison/delite"],
-						packages: [
-							{name: "liaison", location: "liaison-build"}
-						]
+						packages: [{
+							name: "liaison",
+							location: "liaison-build"
+						}]
 					}
 				}
 			}
 		},
- 
+
 		// Config to allow uglify to generate the layer.
 		uglify: {
 			options: {
 				banner: "<%= " + outprop + ".header%>",
-				sourceMap: true				
+				sourceMap: true
 			},
 			dist: {
 				src: "<%= " + outprop + ".modules.abs %>",
@@ -127,15 +182,15 @@ module.exports = function (grunt) {
 		content = content.replace(/\.\.(\/|\\\\)tmp(\/|\\\\)/g, "");
 		grunt.file.write(path, content);
 	});
-	
+
 	grunt.registerTask("filterPluginFiles", function (stringRE) {
 		var rel = grunt.config(outprop + ".plugins.rel");
 		var abs = grunt.config(outprop + ".plugins.abs");
-		
-		function test (path) {
+
+		function test(path) {
 			return !path.match(new RegExp(stringRE));
 		}
-		
+
 		grunt.config(outprop + ".plugins.rel", rel.filter(test));
 		grunt.config(outprop + ".plugins.abs", abs.filter(test));
 	});
@@ -146,128 +201,152 @@ module.exports = function (grunt) {
 			layers = grunt.config(name).layers;
 
 		layers.forEach(function (layer) {
-			switch (layer.name) {
-				case "delite/layer":
-					grunt.task.run("amddepsscan:" + layer.name + ":" + name + ":" + amdloader);
-					break;
-				default:
-					grunt.task.run("amddirscan:" + layer.name + ":" + name + ":" + amdloader);
-					break;				
-				}
-			grunt.task.run("amdserialize:" + layer.name + ":" + name + ":" + outprop);
+			if (layer.name === "delite/layer" ||
+				layer.name === "decor/layer" ||
+				layer.name === "deliteful/layer" ||
+				layer.name === "ecma402/layer") {
+				grunt.task.run("amddepsscan:" + layer.name + ":" + name + ":" + amdloader);
+			} else {
+				grunt.task.run("amddirscan:" + layer.name + ":" + name + ":" + amdloader);
+			}
+			grunt.task.run("amdserialize:" + layer.name + ":" + name + ":" + amdloader + ":" + outprop);
 			grunt.task.run("uglify");
 			grunt.task.run("correctSourceMap:" + layer.name);
 			// Remove references to useless html template before copying plugins files.
 			grunt.task.run("filterPluginFiles:\\.html\\.js$");
 			grunt.task.run("copy:plugins");
 		});
-		
-		libDirs.forEach(function (dir){
-			grunt.task.run("buildLib:"+ dir);
+
+		libDirs.forEach(function (dir) {
+			grunt.task.run("buildLib:" + dir);
 		});
 	});
-	
-	// Update samples
-	function buildSamples (content, path, buildDeps, packages) {
-		var scriptRE = /(<script[^>]*>)(\s*require\s*\(\s*\[[\s\S]*?)(<\/script>)/ig,
-			config = "\trequire.config(" +
-				JSON.stringify({
-					packages: packages || buildDeps.map(function (lib) {
-						return {name: lib, location: lib + "-build"};
-					})
-				}, null, "\t").split("\n").map(function (line, i) {
-					return (i > 0 ? "\t" : "") + line;
-				}).join("\n") +
-				");\n";
 
-		var count = 0;
-		return content.replace(scriptRE, function(match, openTag, content, closeTag){
+	// Generate root file.
+	function getBoot(buildDeps) {
+		var result = "require.config(" +
+			JSON.stringify({
+				packages: buildDeps.map(function (lib) {
+					return {
+						name: lib,
+						location: lib + "-build"
+					};
+				})
+			}, null, "\t") +
+			");\n";
+
+		result += "define(" +
+			JSON.stringify(buildDeps.map(function (lib, i) {
+				return lib + (i === 0 ? "/layer" : "/boot");
+			})) + ", function(){});";
+
+		return result;
+	}
+
+	// Update samples
+	function buildSamples(content, buildDeps) {
+		var scriptRE = /(<script[^>]*>)(\s*require\s*\(\s*\[[\s\S]*?)(<\/script>)/ig;
+		return content.replace(scriptRE, function (match, openTag, content, closeTag) {
 			return openTag + "\n" +
-				// If it is the first require of the file include config.
-				(count++ === 0 ? config : "") +
-				"\trequire(" +
-				JSON.stringify(buildDeps.map(function (lib) {
-					return lib + "/layer";
-				}), null, "\t").split("\n").map(function (line, i) {
-					return (i > 0 ? "\t" : "") + line;
-				}).join("\n") +
-				", function(){" + content + "\n\t});\n\t" + closeTag;
+				"\trequire([\"" + buildDeps[0] + "-build/boot\"], function(){" + content + "\n\t});\n\t" + closeTag;
 		});
 	}
-	
-	grunt.registerTask("buildLib", function(dirName) {
+
+	grunt.registerTask("buildLib", function (dirName) {
 		var ibmDeps = [dirName];
-		
+
 		// Update Bower.json
 		var bower = grunt.file.readJSON(dirName + "/bower.json");
 		bower.name += "-build";
-		
-		var deps = bower.dependencies;
-		for (var dep in deps) {
-			if (libDirs.indexOf(dep) !== -1) {
-				deps[dep + "-build"] = deps[dep];
-				delete deps[dep];
-				ibmDeps.push(dep);
+
+		if (dirName === "decor") {
+			bower.dependencies = {};
+		} else {
+			var deps = bower.dependencies || {};
+			var removeDeps = deps.decor ? ["dcl", "requirejs-dplugins"] : [];
+			removeDeps = removeDeps.concat(deps.delite ? ["requirejs-domready", "requirejs-text", "dojo"] : []);
+			for (var dep in deps) {
+				if (libDirs.indexOf(dep) !== -1) {
+					deps[dep + "-build"] = deps[dep];
+					delete deps[dep];
+					ibmDeps.push(dep);
+				}
+				if (removeDeps.indexOf(dep) !== -1 && deps[dep]) {
+					delete deps[dep];
+				}
 			}
 		}
-		
+
 		bower.devDependencies = {};
 		bower.devDependencies[dirName] = bower.version;
-		
+
 		grunt.file.write(outdir + dirName + "/bower.json", JSON.stringify(bower, null, 2));
-		
+
 		// Copy files
 		grunt.file.copy(dirName + "/LICENSE", dirName + "-build/LICENSE");
 		try {
 			grunt.file.copy(dirName + "/.bowerrc", dirName + "-build/.bowerrc");
-		} catch (e) {
-		}
-		
+		} catch (e) {}
+
 		// Add README.md
 		grunt.file.copy("./README.template", dirName + "-build/README.md", {
 			process: function (template) {
-				return grunt.template.process(template, {data: {project: dirName}});
+				return grunt.template.process(template, {
+					data: {
+						project: dirName
+					}
+				});
 			}
 		});
-			
+
 		// Copy and modify samples.
 		var pathModule = require("path"),
 			config = grunt.config([this.name, dirName, "samples"]),
-			samples = grunt.file.expand({filter: "isFile", cwd: dirName}, (config || {}).src || "samples/**/*"),
+			samples = grunt.file.expand({
+				filter: "isFile",
+				cwd: dirName
+			}, (config || {}).src || "samples/**/*"),
 			fileConfig = {};
 
 		if (config) {
 			Object.keys(config).forEach(function (pattern) {
 				if (pattern !== "excludes") {
-					grunt.file.expand({filter: "isFile", cwd: dirName}, pattern).forEach(function (path) {
+					grunt.file.expand({
+						filter: "isFile",
+						cwd: dirName
+					}, pattern).forEach(function (path) {
 						fileConfig[pathModule.normalize(path)] = config[pattern];
 					});
 				}
 			});
 		}
 
-		samples.forEach(function(path){
+		samples.forEach(function (path) {
 			var orig = dirName + "/" + path;
 			var dest = dirName + "-build/" + path;
 			var sublayersDeps = (fileConfig[pathModule.normalize(path)] || {}).sublayers || [];
 			var packages = (fileConfig[pathModule.normalize(path)] || {}).packages;
 			grunt.file.copy(orig, dest, {
 				noProcess: ["**/*.png", "**/*.js", "**/*.less"],
-				process: function (content, path) {
-					return buildSamples(content, path, ibmDeps.concat(sublayersDeps), packages);
+				process: function (content) {
+					return buildSamples(content, ibmDeps);
 				}
 			});
-		})
-		
+		});
+
+		// Add boot file
+		grunt.file.write(dirName + "-build/boot.js", getBoot(ibmDeps));
+
 		// Copy to final destination
-		var files = grunt.file.expand({filter: "isFile", cwd: outdir + dirName},  "**/*");
-		files.forEach(function(path){
+		var files = grunt.file.expand({
+			filter: "isFile",
+			cwd: outdir + dirName
+		}, "**/*");
+		files.forEach(function (path) {
 			var orig = outdir + dirName + "/" + path;
 			var dest = dirName + "-build/" + path;
 			grunt.file.copy(orig, dest);
 		});
-		
-		
 	});
 
 

28db91e531acb3e2f12701fb16d9573d6a3d3922
Clément Mathieu <clement.mathieu@outlook.com>
2014-10-27 12:04:32 +0100

    Fixes boot of sublayer (like liaison/delite) and enhance readme template

--- build/tasks/addBoot.js
+++ build/tasks/addBoot.js
@@ -7,10 +7,17 @@ module.exports = function (grunt) {
 
 		layers.forEach(function (layer) {
 			var bower = layer.bower;
+			var layerNameBuild = layer.name.replace(/^([^\/]*)\//, "$1-build/");
 			var path = outdir + layer.outputPath;
 
 			// Get built deps
 			var builtDeps = [];
+			// if this is a sublayer, lets add the main layer to the dependency list:
+			var match = layer.name.match(/^([^\/]*)\/[^\/]*\/layer$/);
+			if (match) {
+				builtDeps.push(match[1] + "-build");
+			}
+
 			for (var dep in bower.dependencies) {
 				if (dep.match(/-build$/)) {
 					builtDeps.push(dep);
@@ -20,15 +27,15 @@ module.exports = function (grunt) {
 			// Generate root module.
 			var boot = "\nrequire.config(" +
 				JSON.stringify({
-					packages: [bower.name].concat(builtDeps).map(function (dep) {
+					packages: [layerNameBuild.replace(/\/layer$/, "")].concat(builtDeps).map(function (dep) {
 						return {
-							name: dep.replace(/-build$/, ""),
+							name: dep.replace(/-build/, ""),
 							location: dep
 						};
 					})
 				}, null, "\t") +
 				");\n";
-			boot += "define(\"" + bower.name + "/layer\", " +
+			boot += "define(\"" + layerNameBuild + "\", " +
 				JSON.stringify(builtDeps.map(function (dep) {
 					return dep + "/layer";
 				})) + ", function(){});";
